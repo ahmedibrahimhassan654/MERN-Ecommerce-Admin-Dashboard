ea0d2d3201f88e4e0c7a9a0d2e8fc055f8d86233
unknown <ahmedibrahimhassan654@gmail.com>
2020-12-02 18:00:25 +0200

    create branch component

--- client/src/pages/dashbords/owner/branches/CreatBranch.js
+++ client/src/pages/dashbords/owner/branches/CreatBranch.js
@@ -4,11 +4,34 @@ import PropTypes from "prop-types";
 import OwnernNav from "../../../../components/nav/OwnerNav";
 
 import { useState } from "react";
-import { Col, Layout, Row } from "antd";
+import { Col, Layout, Row ,Form, Input, InputNumber, Button} from "antd";
 
 const { Content, Footer } = Layout;
+const layout = {
+   labelCol: {
+     span: 8,
+   },
+   wrapperCol: {
+     span: 16,
+   },
+ };
+ const onFinish = (values) => {
+   console.log(values);
+ };
+ 
+ const validateMessages = {
+   required: '${label} is required!',
+   types: {
+     email: '${label} is not a valid email!',
+     number: '${label} is not a valid number!',
+   },
+   number: {
+     range: '${label} must be between ${min} and ${max}',
+   },
+ };
 
 function CreatBranch(props) {
+  
   const [formData, setFormData] = useState({
     name: "",
     description: "",
@@ -38,7 +61,8 @@ function CreatBranch(props) {
     documents,
   } = formData;
 
-  return (
+   return (
+     
     <Layout
       className="site-layout"
       style={{
@@ -64,10 +88,46 @@ function CreatBranch(props) {
         >
           <h1 className="text-primary pb-4 pt-5 ">Create New Branch</h1>
           <Row className="container">
-            <Col span={12}>
-              <button type="submit" class="btn btn-primary">
-                Add Branch Information
-              </button>
+                 <Col span={12}>
+                    <Form {...layout} name="nest-messages"
+                       onFinish={onFinish} validateMessages={validateMessages}
+                    >
+      <Form.Item
+        name={['branch', 'name']}
+        label="Branch Name"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name={['branch', 'email']}
+        label="Contact Email"
+        rules={[
+          {
+            type: 'email',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+     
+      <Form.Item name={['user', 'website']} label="Website">
+        <Input />
+      </Form.Item>
+      <Form.Item name={['branch', 'description']} label="Description">
+        <Input.TextArea />
+      </Form.Item>
+      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+             
             </Col>
           </Row>
         </div>
