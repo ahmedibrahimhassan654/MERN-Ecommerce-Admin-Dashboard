cdfdcf1cfed73900e4d831cd71490a9e49cd480a
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-07-15 02:56:26 +0200

    112. Product Card Component

--- client/src/components/cards/OwnerProductCard.js
+++ client/src/components/cards/OwnerProductCard.js
@@ -1,53 +1,51 @@
 import React from 'react'
 import { Card } from 'antd';
-import { EditOutlined,DeleteOutlined } from '@ant-design/icons';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import iamge from '../../components/images/awhite_200319_3944_4.0.0.jpg';
 import { Link } from 'react-router-dom';
 const { Meta } = Card;
-const OwnerProductCard = ({ product ,handleReove}) => {
-    const { title, description, images ,_id} = product
+const OwnerProductCard = ({ product, handleReove }) => {
+    const { title, description, images, _id } = product
     return (
         <div className="space-align-container m-5 ">
             <div className="space-align-block ">
                 <Card
                     hoverable
-                style={{
-                   width: '500',
-                   height: 'auto',
-                  //  backgroundColor: 'black',
-                   border:'5 solid black',
-                }}
+                    style={{
+                        width: '300',
+
+                    }}
                     cover={
                         <img
-                            className='p-1'
-                            style={{
-                                height: 'auto',
-                                objectFit: 'cover'
-                            }}
+
                             alt={title}
-                          src={images && images.length ? images[0].url : iamge}
-                       />
+                            src={images && images.length ? images[0].url : iamge}
+                        />
                     }
-                actions={
-                   [
-                      <div className='row'>
-                         <div className='col-md-6'>
-                              <Link to={`/owner/product/${_id}`}>
-                              <EditOutlined className='text-warning'/>,
-                              </Link>
-                         </div>
-                             <div className='col-md-6'>
-                            <DeleteOutlined onClick={() => handleReove(_id)} className="text-danger" />
-                         </div>
-                   
-                         
-                      
-                      </div>
-                      
-                  ]
-               } 
+                    actions={
+                        [
+                            <div className='row'>
+                                <div className='col-md-6'>
+                                    <Link to={`/owner/product/${_id}`}>
+                                        <EditOutlined className='text-warning' />,
+                                    </Link>
+                                </div>
+                                <div className='col-md-6'>
+                                    <DeleteOutlined onClick={() => handleReove(_id)} className="text-danger" />
+                                </div>
+
+
+
+                            </div>
+
+
+
+
+                        ]
+                    }
                 >
-                    <Meta title={title} description={`${description && description.substring(0, 20)}...`} />
+                    <Meta title={title}
+                        description={`${description && description.substring(0, 20)}...`} />
                 </Card>,
             </div>
 
